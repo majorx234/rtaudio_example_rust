37a52855e5e27ba1f674e37d4e8caea64f873d01
MajorX234 <majorx234@googlemail.com>
2022-11-05 22:25:44 +0100

    Add bandpass weight calculation

--- src/filter/bin/filter.rs
+++ src/filter/bin/filter.rs
@@ -39,10 +39,36 @@ impl FIRFilter {
         for i in -middle..=middle {
             sinc[(i + middle) as usize] = (angular_cutoff * i as f32).sin() / (PI * i as f32);
         }
-        sinc[middle as usize] = 2.0 * cutoff;
+        sinc[middle as usize] = 2.0 * cutoff / new_lp_filter.sample_rate;
         blackman_window.apply(&sinc, &mut new_lp_filter.weights);
 
         new_lp_filter
+    }
+
+    pub fn band_pass(low_cutoff: f32, high_cutoff: f32) -> Self {
+        let mut new_bp_filter = FIRFilter::new();
+        let len = if new_bp_filter.len % 2 == 0 {
+            new_bp_filter.len + 1
+        } else {
+            new_bp_filter.len
+        };
+
+        let mut sinc: Vec<f32> = Vec::with_capacity(len);
+        let angular_low_cutoff = (2.0 * PI * low_cutoff) / new_bp_filter.sample_rate;
+        let angular_high_cutoff = (2.0 * PI * high_cutoff) / new_bp_filter.sample_rate;
+
+        let middle = (len / 2) as isize; // should be odd
+        let blackman_window = window::blackman(len);
+        for i in -middle..=middle {
+            sinc[(i + middle) as usize] = ((angular_high_cutoff * i as f32).sin()
+                - (angular_low_cutoff * i as f32).sin())
+                / (PI * i as f32);
+        }
+        sinc[middle as usize] =
+            1.0 - 2.0 * ((high_cutoff - low_cutoff) / new_bp_filter.sample_rate);
+        blackman_window.apply(&sinc, &mut new_bp_filter.weights);
+
+        new_bp_filter
     }
 }
 
