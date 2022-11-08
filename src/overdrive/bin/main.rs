bdb5cc6d944afbd0e474c3e65a9076dd5ee0c834
MajorX234 <majorx234@googlemail.com>
2022-11-08 01:57:22 +0100

    Fix initialization of output vector

--- src/overdrive/bin/main.rs
+++ src/overdrive/bin/main.rs
@@ -8,7 +8,7 @@ use overdrive::Overdrive;
 
 fn main() {
     let (num_samples, input_data) = read_data();
-    let mut values_data: Vec<f32> = Vec::with_capacity(num_samples);
+    let mut values_data: Vec<f32> = vec![0.0; num_samples];
     let mut my_overdrive = Overdrive::new();
     my_overdrive.process_samples(Some(&input_data), None, Some(&mut values_data), None);
     write_data(&values_data, num_samples);
