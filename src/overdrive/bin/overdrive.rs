493d06b24bb1e824751bbc0256bb7393c5d8960e
MajorX234 <majorx234@googlemail.com>
2022-11-08 02:20:28 +0100

    Fix overdrive calculation

--- src/overdrive/bin/overdrive.rs
+++ src/overdrive/bin/overdrive.rs
@@ -33,26 +33,28 @@ impl Effect for Overdrive {
         }
 
         let symetrical_softclip = |x: f32| {
+            let sign = x.signum();
+            let x = x.abs();
             if 0.0 < x && x < 0.333 {
+                sign * 2.0 * x
+            } else if 0.333 < x && x < 0.666 {
                 let t = 2.0 - 3.0 * x;
-                (3.0 - t * t) / 3.0
+                sign * (3.0 - t * t) / 3.0
             } else {
-                x
+                sign * 1.0
             }
         };
         if let Some(input_l) = input_l {
             if let Some(output_l) = output_l {
                 for (index, xl) in input_l.iter().enumerate() {
-                    let xl = xl.abs();
-                    output_l[index] = symetrical_softclip(xl);
+                    output_l[index] = symetrical_softclip(*xl);
                 }
             }
         }
         if let Some(input_r) = input_r {
             if let Some(output_r) = output_r {
                 for (index, xr) in input_r.iter().enumerate() {
-                    let xr = xr.abs();
-                    output_r[index] = symetrical_softclip(xr);
+                    output_r[index] = symetrical_softclip(*xr);
                 }
             }
         }
