9acbc927c5ee7837e8c15cedccc85e34e1f23ff6
MajorX234 <majorx234@googlemail.com>
2022-11-11 02:58:00 +0100

    Add loop to apply delay on each frame of the input

--- src/delay/bin/main.rs
+++ src/delay/bin/main.rs
@@ -10,6 +10,23 @@ fn main() {
     let (num_samples, input_data) = read_data();
     let mut values_data: Vec<f32> = vec![0.0; num_samples];
     let mut my_delay = Delay::new();
-    my_delay.process_samples(Some(&input_data), None, Some(&mut values_data), None);
+    let mut start_index: usize = 0;
+    while start_index + 1024 < num_samples {
+        my_delay.process_samples(
+            Some(&input_data[start_index..start_index + 1024]),
+            None,
+            Some(&mut values_data[start_index..start_index + 1024]),
+            None,
+        );
+        start_index += 1024;
+    }
+    let rest: usize = num_samples - start_index;
+    my_delay.set_frame_size(rest);
+    my_delay.process_samples(
+        Some(&input_data[start_index..start_index + rest]),
+        None,
+        Some(&mut values_data[start_index..start_index + rest]),
+        None,
+    );
     write_data(&values_data, num_samples);
 }
