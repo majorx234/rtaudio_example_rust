d81a6fcf8dd71bd6709fb25a908feef2fa8ff857
MajorX234 <majorx234@googlemail.com>
2022-11-20 00:39:53 +0100

    Add writing plot into buffer

--- src/plotters_wave/bin/plotters_wave.rs
+++ src/plotters_wave/bin/plotters_wave.rs
@@ -1,9 +1,32 @@
 use plotters::prelude::*;
+use plotters::style::Color;
+use plotters_bitmap::bitmap_pixel::RGBPixel;
+use plotters_bitmap::BitMapBackend;
+use std::error::Error;
 
-pub fn plot(values_data: &Vec<f32>, size: usize) {
+const WIDTH: usize = 800;
+const HEIGHT: usize = 600;
+
+pub fn plot(values_data: &[f32], size: usize) -> Result<(), Box<dyn Error>> {
     let data: Vec<(f32, f32)> = (0..size)
         .map(|x| x as f32)
         .zip(values_data.iter().cloned())
         .collect();
-    print!("{:?}", data);
+    //    print!("{:?}", data);
+    let mut buf = vec![0u8; WIDTH * HEIGHT * 3];
+    let root =
+        BitMapBackend::<RGBPixel>::with_buffer_and_format(&mut buf, (WIDTH as u32, HEIGHT as u32))?
+            .into_drawing_area();
+    root.fill(&BLACK)?;
+
+    let mut chart = ChartBuilder::on(&root)
+        .caption("rtaudio_example", ("Arial", 50).into_font())
+        .margin(5)
+        .set_all_label_area_size(30)
+        .build_cartesian_2d(0.0..(size as f32), -1.0f32..1.0f32)
+        .unwrap();
+
+    chart.configure_mesh().draw()?;
+    chart.draw_series(LineSeries::new(data, &RED)).unwrap();
+    Ok(())
 }
