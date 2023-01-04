bbc93d17bf7d99055c2ff23f5cfb322df7e8b457
MajorX234 <majorx234@googlemail.com>
2023-01-04 20:30:50 +0100

    Add parameter evaluation of adsr parameters

--- src/adsr_modificator/bin/main.rs
+++ src/adsr_modificator/bin/main.rs
@@ -1,14 +1,58 @@
 use rtaudio_lib::read_data;
 use rtaudio_lib::simple_adsr as adsr;
 use rtaudio_lib::write_data;
+use std::env::args;
 
 fn main() {
+    let mut argit = args();
+    let attack = argit.nth(1).clone();
+    let decay = argit.next().clone();
+    let sustain = argit.next().clone();
+    let release = argit.next().clone();
+
+    let attack = if let Some(attack) = attack {
+        if let Ok(attack) = str::parse::<f32>(&attack) {
+            attack
+        } else {
+            panic!("attack isn't given as f32 value");
+        }
+    } else {
+        panic!("No attack argument given");
+    };
+
+    let decay = if let Some(decay) = decay {
+        if let Ok(decay) = str::parse::<f32>(&decay) {
+            decay
+        } else {
+            panic!("decay isn't given as f32 value");
+        }
+    } else {
+        panic!("No decay argument given");
+    };
+
+    let sustain = if let Some(sustain) = sustain {
+        if let Ok(sustain) = str::parse::<f32>(&sustain) {
+            sustain
+        } else {
+            panic!("sustain isn't given as f32 value");
+        }
+    } else {
+        panic!("No sustain argument given");
+    };
+
+    let release = if let Some(release) = release {
+        if let Ok(release) = str::parse::<f32>(&release) {
+            release
+        } else {
+            panic!("release isn't given as f32 value");
+        }
+    } else {
+        panic!("No release argument given");
+    };
+
     let (size, values_data) = read_data();
-    let ta: f32 = 0.05;
-    let td: f32 = 0.25;
-    let ts: f32 = 0.6;
-    let tr: f32 = 0.1;
-    let adsr_modificator: Vec<f32> = adsr::generate_adsr_modificator(size, ta, td, ts, tr);
+    let adsr_modificator: Vec<f32> =
+        adsr::generate_adsr_modificator(size, attack, decay, sustain, release);
     let values_data: Vec<f32> = adsr::adsr_multiplication(adsr_modificator, values_data, size);
     write_data(&values_data, size);
 }
