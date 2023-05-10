074c93767a8944a8269e0554f979466e54fe3f38
MajorX234 <majorx234@googlemail.com>
2023-05-10 23:53:13 +0200

    Add wave file reading in sample play

--- src/sample_play/bin/sample_wave.rs
+++ src/sample_play/bin/sample_wave.rs
@@ -1,4 +1,4 @@
-use crate::wave::Wave;
+use rtaudio_lib::wave::Wave;
 use rtaudio_lib::write_data;
 use std::f32;
 use std::fs::File;
@@ -13,15 +13,22 @@ pub struct SampleWave {
 
 impl SampleWave {
     pub fn new(filename: &String) -> SampleWave {
-        let num_samples: usize = 48000;
         let fsample_rate: f32 = 48000.0;
         let mut wave_file = File::open(Path::new(filename)).unwrap();
         let (header, data) = wav::read(&mut wave_file).unwrap();
 
-        let values_data = vec![0.0; num_samples];
+        //let values_data = vec![0.0; num_samples];
+        let values_data = if let Some(values_data) = data.as_thirty_two_float() {
+            values_data
+        } else {
+            return SampleWave {
+                num_samples: 0,
+                values: vec![],
+            };
+        };
         return SampleWave {
-            num_samples: num_samples,
-            values: values_data,
+            num_samples: values_data.len(),
+            values: (*values_data).to_vec(),
         };
     }
 }
