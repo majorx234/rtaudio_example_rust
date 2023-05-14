4c17e39724faaf5a47e0f52163834f4356df22eb
MajorX234 <majorx234@googlemail.com>
2023-05-14 19:31:42 +0200

    Add reverb executeable

--- src/reverb/bin/main.rs
+++ src/reverb/bin/main.rs
@@ -1,1 +1,12 @@
+use rtaudio_lib::effect::Effect;
+use rtaudio_lib::read_data;
 use rtaudio_lib::reverb::Reverb;
+use rtaudio_lib::write_data;
+
+fn main() {
+    let (num_samples, input_data) = read_data();
+    let mut values_data: Vec<f32> = vec![0.0; num_samples];
+    let mut my_reverb = Reverb::new();
+    my_reverb.process_samples(Some(&input_data), None, Some(&mut values_data), None);
+    write_data(&values_data);
+}
